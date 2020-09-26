b1c2840a7e49761944b9d0e41c804ff1205cf352
BinarySteve <itsstevechav@gmail.com>
2020-09-26 00:14:37 -0500

    fixed delete function

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,37 +1,37 @@
-import React from "react";
-import "../styles/css/Product.css";
+import React from 'react';
+import '../styles/css/Product.css';
 
-import Trash from "../../node_modules/bootstrap-icons/icons/trash2-fill.svg";
+import Trash from '../../node_modules/bootstrap-icons/icons/trash2-fill.svg';
 
-function Product({  name, cost, quantity, deleteProduct, updateProduct }) {
-  return (
-    <tbody className="text-center">
-      <tr>
-        <td>{quantity}</td>
-        <td className="text-capitalize">
-          {name}
-          <div>(${cost})</div>
-        </td>
-        <td className="">
-          <div> ${(cost * quantity).toFixed(2)}</div>
-        </td>
-        <td>
-          {" "}
-          <img
-            className="trash "
-            onClick={() => {
-              if (window.confirm(`Delete ${name}?`)) {
-                deleteProduct();
-              }
-            }}
-            src={Trash}
-            style={{ cursor: "pointer" }}
-            alt=""
-          />
-        </td>
-      </tr>
-    </tbody>
-  );
+function Product({ id, name, cost, quantity, deleteProduct, updateProduct }) {
+	return (
+		<tbody className="text-center">
+			<tr>
+				<td>{quantity}</td>
+				<td className="text-capitalize">
+					{name}
+					<div>(${cost})</div>
+				</td>
+				<td className="">
+					<div> ${(cost * quantity).toFixed(2)}</div>
+				</td>
+				<td>
+					{' '}
+					<img
+						className="trash "
+						onClick={() => {
+							if (window.confirm(`Delete ${name}?`)) {
+								deleteProduct(id);
+							}
+						}}
+						src={Trash}
+						style={{ cursor: 'pointer' }}
+						alt=""
+					/>
+				</td>
+			</tr>
+		</tbody>
+	);
 }
 
 export default Product;
